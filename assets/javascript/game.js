552d1b844f5c421f3c4e713d350b4688223ede42
Jessica Sills <jsills3110@gmail.com>
2019-09-15 21:16:33 -0700

    Changed the size of all character images. Clicking on a 'side' choice on the opening screen now displays the corresponding heroes or villains.

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var heroes = ["frodobaggins", "samwisegamgee", "gandalf", "aragorn", "legolas", "gimli"];
+var villains = ["nazgul", "saruman", "sauron", "gollum", "shelob"];
+var playerTeam = "";
+
+var wins = 0;
+var losses = 0;
+
+$(".start-button").on("click", function() {
+    chooseCharacterHeader();
+    if (this.value === "Take the Ring") {
+        playerTeam = "villains";
+    } else {
+        playerTeam = "heroes";
+    }
+    showPlayableCharacters();
+});
+
+function chooseCharacterHeader() {
+    $("header").empty();
+    $("#button-holder").empty();
+
+}
+
+function showPlayableCharacters() {
+    if (playerTeam === "villains") {
+        for (var i = 0; i < villains.length; i++) {
+            var image = $("<img>");
+            image.attr("src", "assets/images/" + villains[i] + ".png");
+            $("#choose-your-character").append(image);
+        }
+    } else {
+        for (var i = 0; i < heroes.length; i++) {
+            var image = $("<img>");
+            image.attr("src", "assets/images/" + heroes[i] + ".png");
+            $("#choose-your-character").append(image);
+        }
+    }
+}
