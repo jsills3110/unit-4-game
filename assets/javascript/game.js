50c613c08a64e9f2fd04eb1d3495fe928a3c6ddd
Jessica Sills <jsills3110@gmail.com>
2019-09-18 22:24:33 -0700

    Resized the character pngs again. Background image changes when a new enemy is selected. Commented javascript.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,13 @@
+// Each of the following objects represents a character that the player can 
+// play as or fight against.
 var frodo = {
     name: "Frodo Baggins",
     id: "frodobaggins",
     image: "assets/images/frodobaggins.png",
-    health: 100,
-    currentPower: 5,
-    attackPower: 5,
-    counterPower: 5
+    health: 120,
+    currentPower: 6,
+    attackPower: 6,
+    counterPower: 8
 };
 
 var gandalf = {
@@ -13,8 +15,8 @@ var gandalf = {
     id: "gandalf",
     image: "assets/images/gandalf.png",
     health: 180,
-    currentPower: 8,
-    attackPower: 8,
+    currentPower: 7,
+    attackPower: 7,
     counterPower: 25
 }
 
@@ -23,8 +25,8 @@ var aragorn = {
     id: "aragorn",
     image: "assets/images/aragorn.png",
     health: 150,
-    currentPower: 6,
-    attackPower: 6,
+    currentPower: 7,
+    attackPower: 7,
     counterPower: 20
 }
 
@@ -32,10 +34,10 @@ var nazgul = {
     name: "Witch-king of Angmar",
     id: "nazgul",
     image: "assets/images/nazgul.png",
-    health: 100,
-    currentPower: 5,
-    attackPower: 5,
-    counterPower: 5
+    health: 120,
+    currentPower: 6,
+    attackPower: 6,
+    counterPower: 8
 }
 
 var saruman = {
@@ -43,8 +45,8 @@ var saruman = {
     id: "saruman",
     image: "assets/images/saruman.png",
     health: 150,
-    currentPower: 6,
-    attackPower: 6,
+    currentPower: 7,
+    attackPower: 7,
     counterPower: 20
 }
 
@@ -53,17 +55,21 @@ var sauron = {
     id: "sauron",
     image: "assets/images/sauron.png",
     health: 180,
-    currentPower: 8,
-    attackPower: 8,
+    currentPower: 7,
+    attackPower: 7,
     counterPower: 25
 }
 
-// var heroes = ["frodobaggins", "samwisegamgee", "gandalf", "aragorn", "legolas"];
+// The two groupings of playable characters are heroes and villains.
 var heroes = [frodo, aragorn, gandalf];
-// var villains = ["nazgul", "saruman", "sauron", "gollum", "shelob"];
 var villains = [nazgul, saruman, sauron];
-var playerTeam = "";
 
+// Store the backgrounds in an array so we can randomly pick one with each 
+// new enemy fight.
+var backgrounds = ["hobbiton.jpeg", "minesofmoria.jpg", "ministirith.jpg", "mistymountains.jpg", "mordor.jpg", "rohan.jpg", "theblackgate.jpg", "theshire.jpg"];
+
+// Initialize other useful global variables.
+var playerTeam = "";
 var wins = 0;
 var losses = 0;
 var enemyChosen = false;
@@ -85,6 +91,7 @@ var friendsHolder = $("#friends");
 var enemiesHolder = $("#enemies");
 var messageLog = $("<div>");
 var attackButton = $("<button>");
+var bodyHolder = $("body");
 
 // At the start of the game, players can pick either the heroes or the villains.
 // When they make a choice, the next screen displays their playable characters.
@@ -106,7 +113,6 @@ $(".start-button").on("click", function () {
 // After the player makes a choice about which side they are on, their playable
 // characters are displayed.
 function showPlayableCharacters() {
-
     for (var i = 0; i < friendlyCharacters.length; i++) {
         var image = $("<img>");
         image.attr("src", friendlyCharacters[i].image);
@@ -120,7 +126,6 @@ function showPlayableCharacters() {
 // Once a player has chosen a character by clicking on the image, append the image
 // to the game board and display their enemies.
 function chooseCharacter(theCharacter) {
-
     // Grab the chosen character and set it to playerCharacter variable.
     for (var i = 0; i < friendlyCharacters.length; i++) {
         if (theCharacter === friendlyCharacters[i].id) {
@@ -140,7 +145,6 @@ function chooseCharacter(theCharacter) {
 
 // Display the enemies that the player has to choose from.
 function displayEnemies() {
-
     enemiesHolder.empty();
 
     for (var i = 0; i < enemyCharacters.length; i++) {
@@ -171,11 +175,16 @@ function chooseEnemy(theEnemy) {
             }
         }
 
+        // Select a new background image.
+        var newBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+        $("body").css({"background": "url('assets/images/" + newBackground + "') no-repeat center center fixed", "background-size": "cover"});
+ 
         displayEnemies();
         headerHolder.html("<h1>You are fighting " + currentEnemy.name + "!</h1>");
     }
 }
 
+// Display the Attack button, Message Log box, and reset button.
 function displayOptions() {
     attackButton.attr("id", "#attack");
     attackButton.attr("value", "attack");
@@ -185,7 +194,7 @@ function displayOptions() {
     playerOptionsHolder.append(attackButton);
 
     messageLog.attr("id", "#message-log");
-    messageLog.attr("style", "background-color: wheat; width: 20em; height: 15em; margin: 1em 0; overflow: auto; text-align: center");
+    messageLog.attr("style", "background-color: wheat; width: 20em; height: 8em; margin: 1em 0; overflow: auto; text-align: center");
     playerOptionsHolder.append(messageLog);
 
     var resetButton = $("<button>");
@@ -198,31 +207,41 @@ function displayOptions() {
     playerOptionsHolder.append(resetButton);
 }
 
+// When the attack button is clicked, attack the enemy.
 function attackEnemy() {
+    // First check that an enemy is actually selected.
     if (!enemyChosen) {
         messageLog.prepend("<p>There is no enemy to attack yet.</p>");
     } else {
-        currentEnemy.health -= playerCharacter.currentPower;
-        if (currentEnemy.health <= 0) {
+        currentEnemy.health -= playerCharacter.currentPower; // Reduce the enemy health.
+        if (currentEnemy.health <= 0) { // If the enemy has died...
+            // Remove the enemy from the enemyCharacters array.
             enemyCharacters.splice(enemyCharacters.indexOf(currentEnemy), 1);
             enemyChosen = false;
-            enemyCharacterHolder.empty();
             headerHolder.html("<h1>You killed " + currentEnemy.name + "!</h1>");
             displayEnemies();
-        } else {
-            playerCharacter.health -= currentEnemy.counterPower;
-            if (playerCharacter.health <= 0) {
+            
+            // If there are no more enemies to fight...
+            if (enemyCharacters === undefined || enemyCharacters.length <= 0) {
+                headerHolder.html("<h1>You won the game! Click Reset Game to play again.</h1>");
+                attackButton.attr("disabled", true);
+                attackButton.attr("style", "background-color: grey; margin: 0;");
+            }
+        } else { // If the enemy has not died yet...
+            playerCharacter.health -= currentEnemy.counterPower; // Reduce the player health.
+            if (playerCharacter.health <= 0) { // If the player has died...
                 headerHolder.html("<h1>You lost the game! Click Reset Game to try again.</h1>");
                 playerCharacterHolder.empty();
                 attackButton.attr("disabled", true);
                 attackButton.attr("style", "background-color: grey; margin: 0;");
             }
         }
+        // Add details to the message log.
         messageLog.prepend("<p>You attacked " + currentEnemy.name + " for " + playerCharacter.currentPower + " damage!</p>" +
         "<p>" + currentEnemy.name + " attacked you for " + currentEnemy.counterPower + " damage!</p>" +
         "<p>" + currentEnemy.name + "'s health is at " + currentEnemy.health + ".</p>" +
         "<p>Your health is at " + playerCharacter.health + ".</p><br>");
         
-        playerCharacter.currentPower += playerCharacter.attackPower;
+        playerCharacter.currentPower += playerCharacter.attackPower; // Add to the player's attack power.
     }
 }
